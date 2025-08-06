e81710bcef1a9436e9a57aa9ba92780d932d004a
Venkata-Balaji-22 <s.v.balaji004@gmail.com>
2025-08-07 02:12:10 +0530

    Update Login.jsx

--- frontend/sampleapp/src/Login.jsx
+++ frontend/sampleapp/src/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:5000/api/users/login', {
+      const response = await fetch('https://petservice-wx2h.onrender.com/api/users/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
