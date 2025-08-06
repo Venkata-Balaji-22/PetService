cd9352185743ae45fc91fa190017f7d15e6a024b
Venkata-Balaji-22 <s.v.balaji004@gmail.com>
2025-08-07 02:11:37 +0530

    Update GroomingBookingForm..jsx

--- frontend/sampleapp/src/GroomingBookingForm..jsx
+++ frontend/sampleapp/src/GroomingBookingForm..jsx
@@ -19,7 +19,7 @@ const GroomingBookingForm = () => {
         };
 
         try {
-            const response = await axios.post('http://localhost:5000/api/bookings', bookingData);
+            const response = await axios.post('https://petservice-wx2h.onrender.com/api/bookings', bookingData);
             console.log('Booking request successful:', response.data);
             setSuccessMessage('Your grooming service request has been submitted successfully!'); // Show success message
             // Clear form fields
