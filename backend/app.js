ee6a1b008641e37ec21a1191098479ecf96a4f94
Venkata-Balaji-22 <s.v.balaji004@gmail.com>
2025-08-07 00:38:33 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://petservice-frontend.onrender.com'],
+  origin: ['http://localhost:3000', 'https://PetService1.onrender.com'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
