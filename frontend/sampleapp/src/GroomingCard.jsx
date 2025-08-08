d88577a6495aa34c5a155eb15875f7e804ac4356
Venkata-Balaji-22 <s.v.balaji004@gmail.com>
2025-08-09 00:20:15 +0530

    Update GroomingCard.jsx

--- frontend/sampleapp/src/GroomingCard.jsx
+++ frontend/sampleapp/src/GroomingCard.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './grooming.css'
 const GroomingCard = ({ img, title, description , link}) => {
+      const navigate = useNavigate();
+    
+        const handleBookNow = () => {
+           
+            navigate('/groomingBookingForm', { state: { serviceType: title } }); // Pass the name as state
+        };
     return (
         <div className="grooming-card">
             <img src={img} alt={title} />
             <h3>{title}</h3>
             <p>{description}</p>
-            <a href={link}>Book now</a>
+            <button type="button" onClick={handleBookNow}>Book Now</button>
         </div>
     );
 };
