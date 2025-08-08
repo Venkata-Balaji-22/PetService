f206a60e8af89ee9ede8bc8311f88d10624d3d08
Venkata-Balaji-22 <s.v.balaji004@gmail.com>
2025-08-09 00:22:32 +0530

    Update PetProductCard.jsx

--- frontend/sampleapp/src/PetProductCard.jsx
+++ frontend/sampleapp/src/PetProductCard.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './PetProduct.css';
 
 const PetProductCard = ({ image, name, description ,link}) => {
+    const navigate = useNavigate();
+
+    const handleBookNow = () => {
+        navigate('/ProductForm', { state: { serviceType: name } }); // Pass the name as state
+    };
     return (
         <div className="product-card">
             <img src={image} alt={name} />
             <h3>{name}</h3>
             <p>{description}</p>
-            <a href={link}>Book now</a>
+            <button type="button" onClick={handleBookNow}>Book Now</button>
         </div>
     );
 };
