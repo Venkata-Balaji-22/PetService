db775ba3281b8b33412dd5869ae514e9f5dc85c9
Venkata-Balaji-22 <s.v.balaji004@gmail.com>
2025-08-09 00:24:48 +0530

    Update ServiceCard.jsx

--- frontend/sampleapp/src/ServiceCard.jsx
+++ frontend/sampleapp/src/ServiceCard.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './ServiceCard.css';
 
 const ServiceCard = ({ img, title, description, link }) => {
+    const navigate = useNavigate();
+    
+    const handleClick = (e) => {
+        e.preventDefault();  // Prevent <a> tag's default behavior
+        navigate('/serviceform', { state: { serviceType: title } });
+    };
     return (
         <div className="service-card">
             <img src={img} alt={title} />
             <h3>{title}</h3>
             <p>{description}</p>
-            <a href={link}>Book Service</a>
+            {/* <a href={link} onClick={navigate('/serviceform', { state: { serviceType: title } })}>Book Service</a> */}
+             <button type="button" onClick={handleClick}>Book Service</button>
+            {/* <button type='button' onClick={navigate('/serviceform', { state: { serviceType: title } })}>Book Service</button> */}
         </div>
     );
 };
